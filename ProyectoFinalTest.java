6cee3d4e60d8128ec15b0e271faff779a65fa2cb
Hector Delgado <hadeo10@gmail.com>
2012-07-25 07:47:55 -0500

    Iniciando el test

---
+++ ProyectoFinalTest.java
@@ -0,0 +1,29 @@
+
+package proyectofinal;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author hector
+ */
+public class ProyectoFinalTest {
+    
+    public ProyectoFinalTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+}
